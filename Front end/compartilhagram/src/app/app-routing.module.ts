afb6adbfc2a8e2343153953dde6a84f4f96b182f
danielagff <daniel.agff@gmail.com>
2021-05-10 12:13:23 -0300

    Criação da navBar, login e cadastro

--- Front end/compartilhagram/src/app/app-routing.module.ts
+++ Front end/compartilhagram/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { FeedComponent } from './feed/feed.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'login', component: LoginComponent},
+{path: 'feed', component: FeedComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Front end/compartilhagram/src/app/app.module.ts
+++ Front end/compartilhagram/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FeedComponent } from './feed/feed.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    FeedComponent,
+    CadastroComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
